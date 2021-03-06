64593b528b1f516867df85a8beb2ef73fea21b96
pariwesh009 <ajay2322020@outlook.com>
2021-03-06 09:51:29 +0530

    arguments

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const { graphqlHTTP } = require('express-graphql');
-const {graphql, buildSchema} = require('graphql');
+const { graphql, buildSchema } = require('graphql');
 const express = require('express');
 const schema = buildSchema(
-  `  type Query {
-        product: String
+    `  type Query {
+        product: String,
+        getProducts(length:Int): [Int]
     }`
 );
 
-const root = {product : ()=> "laptop"}
+const root = { //controller
+    product: () => "laptop",  //controller method
+    getProducts: (argu)=> {
+        //DB
+        console.log(argu)
+        return [1,2,3].splice(0, argu.length)
+    }
+}
 
 var server = express();
 server.use('/graphql', graphqlHTTP({
-    schema:schema,
+    schema: schema,
     rootValue: root,
-    graphiql:true
+    graphiql: true
 }));
 server.listen(4000);
 // graphql(schema, '{product}', root).then(response => console.log(response))
